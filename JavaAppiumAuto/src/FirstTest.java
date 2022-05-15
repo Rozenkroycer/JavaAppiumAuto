753afec27153fe662b56db0e8f7b8b9989a8cac6
Rozenkroycer <pavelbulic@gmail.com>
2022-05-15 21:15:16 +0200

    Homework4_Ex7

--- JavaAppiumAuto/src/FirstTest.java
+++ JavaAppiumAuto/src/FirstTest.java
@@ -36,7 +36,7 @@ public class FirstTest {
         capabilities.setCapability("app", "/Users/pavelbulic/Documents/GitHub/JavaAppiumAuto/JavaAppiumAuto/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        //driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
 
